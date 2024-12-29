1b880926fb86ed532258bab49ae7cb275103d707
Hashray_10 <hashraykumbhani258@gmail.com>
2024-12-29 20:27:27 +0530

    Update app.js

--- app.js
+++ app.js
@@ -13,6 +13,10 @@ const app=express();
  
 app.set("view engine","ejs");
 app.use(express.static(path.join(__dirname,"public")));
+app.use((req, res, next) => {
+  console.log(`Incoming request: ${req.method} ${req.url}`);
+  next();
+});
 
 app.get("/:shopname",(req,res)=>{
     const shopName=req.params.shopname;
